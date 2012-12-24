79070bbedf8f33201127e495a8c15af7fe7da3bb
Rico Sta. Cruz <rico@ambiescent.com>
2012-12-25 04:30:58 +0800

    Add support for tap-to-play.

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 
     var lastID = 0;
 
-    $('.example').live('mouseenter play', function() {
+    $('.example').live('play', function() {
       var $example = $(this).closest('.example');
       $example.trigger('reset');
 
@@ -24,7 +24,7 @@
       eval(code);
     });
 
-    $('.example').live('mouseleave reset', function() {
+    $('.example').live('reset', function() {
       var $example = $(this).closest('.example');
       var $ghost = $example.find('.ghost');
 
@@ -35,6 +35,24 @@
 
       $example.removeClass('playing');
     }); 
+
+    var isMobile = (!! window.navigator.userAgent.match(/iPod|iPad|iPhone/));
+
+    // For iPad and such, tap to play
+    if (isMobile) {
+      $('.example').live('click', function() {
+        $('.example.playing').trigger('reset');
+        $(this).trigger('play');
+      });
+    }
+
+    // For desktops, hover
+    else {
+      $('.example')
+        .live('mouseenter', function() { $(this).trigger('play'); })
+        .live('mouseleave', function() { $(this).trigger('reset'); });
+    }
+
 
     // Equalizer:
     // Ensure things line up on any browser width.
@@ -57,5 +75,10 @@
     $(function() {
       $(window).trigger('reequalize');
     });
+
+    // Export
+    window.App = {
+      isMobile: isMobile
+    };
 
 })();
